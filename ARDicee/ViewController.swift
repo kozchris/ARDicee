1634611c97c94d57c9783b648ff1d73f58da12be
Chris Snyder <csnyder@mind-tap.net>
2018-11-27 18:24:46 -0500

    initial project with moon

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -21,13 +21,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        //sceneView.showsStatistics = true
+        
+        //let object = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        let object = SCNSphere(radius: 0.2)
+        let material = SCNMaterial()
+        //material.diffuse.contents = UIColor.red
+        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
+        object.materials = [material]
+        
+        let node = SCNNode()
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        node.geometry = object
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
+        //sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,6 +51,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
+        //print("AR World Tracking is supported: \(ARWorldTrackingConfiguration.isSupported)")
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
